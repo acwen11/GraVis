8f566dbe0c12dbd73f57d87089595f6563599110
Allen Wen <acw6923@g.rit.edu>
2025-01-29 02:04:37 -0500

    bug fix in Cactus reader

--- GraVis/DataInterface/CreateFileCactus.py
+++ GraVis/DataInterface/CreateFileCactus.py
@@ -20,7 +20,7 @@ transformVectorData = True
 transformRhoData = False
 
 
-with open('metaData' + '.meta', 'wb') as metaData:
+with open('metaData_noleak' + '.meta', 'wb') as metaData:
 	metaData.write(bytearray(struct.pack("<i", 0))) # enum type
 	metaData.write(bytearray(struct.pack("<i", 4)))  # how many variables: rho, press, velocity, B
 	name = "Rho".encode('utf-8')
@@ -92,30 +92,55 @@ def time_convert(sec):
 start_time_total = time.time()
 
 if (transformRhoData):
-	rangelist = list(range(0, total))
+	print("Reading Vector Data")
+	with SimDir(
+		datadir,
+		pickle_file=pickle_file,
+	) as sim:
+		reader = sim.gridfunctions['xyz']
+		rho = reader["rho_b"]
+		print("Read rho")
+
+	rangelist = list(range(0,total))
 	for i in rangelist:
+		print("Reading output {:d} of {:d}.\n".format(i, total))
+		iteration = i * 8192
 		start_time_sample = time.time()
-		data = athena_read.athdf(source_path + "gr_mhd_tov.out1." + getIndexString(i) + ".athdf", raw=True)
-		values = [0.0] * 256 * 256 * 256
-		with open('otdata/Rho/Rho' + getIndexString(i) + '.otdata', 'wb') as rhoData:
-			for blockID in range(0, 4096):
-				for x in range(0, 16):
-					for y in range(0, 16):
-						for z in range(0, 16):
-							value = data["rho"][blockID][x][y][z]
-							level = data["LogicalLocations"][blockID]
+		#data = athena_read.athdf(source_path + "gr_mhd_tov.out1."+getIndexString(i)+".athdf", raw=True)
 
-							_x = level[0] * 16 + z
-							_y = level[1] * 16 + y
-							_z = level[2] * 16 + x
-							index = _x + 256 * _y + 256 * 256 * _z
-							values[index] = value
-				# for first row: x, so + 0
-				# for second row: y, so + 1
-				# for third row: z, so + 2
+		rhodata = rho[iteration].to_UniformGridData(shape, x0, x1).data.flatten()
 
-			dataList = np.array(values).ravel()
+		# values = [0.0] * 256*256*256*3
+		dataList = rhodata.ravel()
+
+		with open('otdata_noleak/Rho/Rho' + getIndexString(i) + '_MIP0.otdata', 'wb') as rhoData:
 			rhoData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
+
+		# Athena stuff vvvvv
+	# rangelist = list(range(0, total))
+	# for i in rangelist:
+	# 	start_time_sample = time.time()
+	# 	data = athena_read.athdf(source_path + "gr_mhd_tov.out1." + getIndexString(i) + ".athdf", raw=True)
+	# 	values = [0.0] * 256 * 256 * 256
+	# 	with open('otdata/Rho/Rho' + getIndexString(i) + '.otdata', 'wb') as rhoData:
+	# 		for blockID in range(0, 4096):
+	# 			for x in range(0, 16):
+	# 				for y in range(0, 16):
+	# 					for z in range(0, 16):
+	# 						value = data["rho"][blockID][x][y][z]
+	# 						level = data["LogicalLocations"][blockID]
+
+	# 						_x = level[0] * 16 + z
+	# 						_y = level[1] * 16 + y
+	# 						_z = level[2] * 16 + x
+	# 						index = _x + 256 * _y + 256 * 256 * _z
+	# 						values[index] = value
+	# 			# for first row: x, so + 0
+	# 			# for second row: y, so + 1
+	# 			# for third row: z, so + 2
+
+	# 		dataList = np.array(values).ravel()
+	# 		rhoData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
 
 		timeTaken = time.time() - start_time_sample
 		totalTime = time.time() - start_time_total
@@ -123,6 +148,7 @@ if (transformRhoData):
 		print("Data completed in " + time_convert(timeTaken))
 		print("Total time: " + time_convert(totalTime) + ". Time left: " + time_convert(timeLeft))
 		print("Datasets done by " + str((i + 1) / (total) * 100) + "%")
+		rho.clear_cache()
 
 
 if (transformVectorData):
@@ -154,9 +180,9 @@ if (transformVectorData):
 		values[1] = Bydata
 		values[2] = Bzdata
 		values = values.T.flatten() # convert to 1D [ Bx(0,0,0), By(0,0,0), Bz(0,0,0), ... ]
-		datalist = values.ravel()
+		dataList = values.ravel()
 
-		with open('otdata/B/B' + getIndexString(i) + '.otdata', 'wb') as vectorData:
+		with open('otdata_noleak/B/B' + getIndexString(i) + '_MIP0.otdata', 'wb') as vectorData:
 			vectorData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
 
 		# Athena stuff vvvvv
@@ -215,6 +241,10 @@ if (transformVectorData):
 		print("Data completed in "+time_convert(timeTaken))
 		print("Total time: " + time_convert(totalTime)+". Time left: " + time_convert(timeLeft))
 		print("Datasets done by " + str((i+1) / (total) * 100) + "%")
+		Bx.clear_cache()
+		By.clear_cache()
+		Bz.clear_cache()
+
 # single data sets
 #with open('rhoData' + str(data["Time"]) + '.txt', 'wb') as rhoData:
 #	 rhoData.write(bytearray(struct.pack("<i", 1)))
