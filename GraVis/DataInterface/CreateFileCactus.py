9d70224b2e97a61119223892d577a3c8226dacf0
Allen Wen <acw6923@g.rit.edu>
2025-04-01 11:54:16 -0400

    clean up and add comments to cactus reader

--- GraVis/DataInterface/CreateFileCactus.py
+++ GraVis/DataInterface/CreateFileCactus.py
@@ -1,8 +1,6 @@
-
-# use athena_read script to read the hdf-files
+# use script to read 3D hdf-files
 import numpy as np
 
-# import athena_read
 from kuibit.simdir import SimDir
 import numpy
 import struct
@@ -11,16 +9,20 @@ import time
 datadir = "/lagoon/allenwen/LDP_LR_noleak" # "E:/SimulationData/data/"
 pickle_file = "/lagoon/allenwen/LDP_LR_noleak.pickle" # "E:/SimulationData/data/"
 shape = [256, 256, 256]
+
+# The code can currently only handle data in a 256^3 point grid. Set the corners of the cube here.
 x0 = [-24, -24, -24]
 x1 = [24, 24, 24]
 
 
-total = 124 #1798 is the max amount of data sets
+total = 124 # Number of 3D snapshots. 1798 is the max
+
+# Enable to generate scalar or vector output. Rho isosurface rendering is currently broken but the code requires these files to exist anyway.
 transformVectorData = True 
-transformRhoData = False
+transformRhoData = True
 
 
-with open('metaData_noleak' + '.meta', 'wb') as metaData:
+with open('metaData' + '.meta', 'wb') as metaData:
 	metaData.write(bytearray(struct.pack("<i", 0))) # enum type
 	metaData.write(bytearray(struct.pack("<i", 4)))  # how many variables: rho, press, velocity, B
 	name = "Rho".encode('utf-8')
@@ -106,41 +108,13 @@ if (transformRhoData):
 		print("Reading output {:d} of {:d}.\n".format(i, total))
 		iteration = i * 8192
 		start_time_sample = time.time()
-		#data = athena_read.athdf(source_path + "gr_mhd_tov.out1."+getIndexString(i)+".athdf", raw=True)
 
 		rhodata = rho[iteration].to_UniformGridData(shape, x0, x1).data.flatten()
 
-		# values = [0.0] * 256*256*256*3
 		dataList = rhodata.ravel()
 
-		with open('otdata_noleak/Rho/Rho' + getIndexString(i) + '_MIP0.otdata', 'wb') as rhoData:
+		with open('otdata/Rho/Rho' + getIndexString(i) + '_MIP0.otdata', 'wb') as rhoData:
 			rhoData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
-
-		# Athena stuff vvvvv
-	# rangelist = list(range(0, total))
-	# for i in rangelist:
-	# 	start_time_sample = time.time()
-	# 	data = athena_read.athdf(source_path + "gr_mhd_tov.out1." + getIndexString(i) + ".athdf", raw=True)
-	# 	values = [0.0] * 256 * 256 * 256
-	# 	with open('otdata/Rho/Rho' + getIndexString(i) + '.otdata', 'wb') as rhoData:
-	# 		for blockID in range(0, 4096):
-	# 			for x in range(0, 16):
-	# 				for y in range(0, 16):
-	# 					for z in range(0, 16):
-	# 						value = data["rho"][blockID][x][y][z]
-	# 						level = data["LogicalLocations"][blockID]
-
-	# 						_x = level[0] * 16 + z
-	# 						_y = level[1] * 16 + y
-	# 						_z = level[2] * 16 + x
-	# 						index = _x + 256 * _y + 256 * 256 * _z
-	# 						values[index] = value
-	# 			# for first row: x, so + 0
-	# 			# for second row: y, so + 1
-	# 			# for third row: z, so + 2
-
-	# 		dataList = np.array(values).ravel()
-	# 		rhoData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
 
 		timeTaken = time.time() - start_time_sample
 		totalTime = time.time() - start_time_total
@@ -168,13 +142,11 @@ if (transformVectorData):
 		print("Reading output {:d} of {:d}.\n".format(i, total))
 		iteration = i * 8192
 		start_time_sample = time.time()
-		#data = athena_read.athdf(source_path + "gr_mhd_tov.out1."+getIndexString(i)+".athdf", raw=True)
 
 		Bxdata = Bx[iteration].to_UniformGridData(shape, x0, x1).data.flatten()
 		Bydata = By[iteration].to_UniformGridData(shape, x0, x1).data.flatten()
 		Bzdata = Bz[iteration].to_UniformGridData(shape, x0, x1).data.flatten()
 
-		# values = [0.0] * 256*256*256*3
 		values = np.empty((3, 256*256*256))
 		values[0] = Bxdata
 		values[1] = Bydata
@@ -182,59 +154,9 @@ if (transformVectorData):
 		values = values.T.flatten() # convert to 1D [ Bx(0,0,0), By(0,0,0), Bz(0,0,0), ... ]
 		dataList = values.ravel()
 
-		with open('otdata_noleak/B/B' + getIndexString(i) + '_MIP0.otdata', 'wb') as vectorData:
+		with open('otdata/B/B' + getIndexString(i) + '_MIP0.otdata', 'wb') as vectorData:
 			vectorData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
 
-		# Athena stuff vvvvv
-		#with open('otdata/B/B' + getIndexString(i) + '.otdata', 'wb') as vectorData:
-		# for blockID in range(0, 4096):
-		#	  for x in range(0, 16):
-		#		  for y in range(0, 16):
-		#			  for z in range(0, 16):
-		#				  _x = level[0] * 16 + z
-		#				  _y = level[1] * 16 + y
-		#				  _z = level[2] * 16 + x
-		#				  index = _x + 256 * _y + 256 * 256 * _z
-		#				  values[index * 3] = data["Bcc1"][blockID][x][y][z]
-		#				  values[index * 3 + 1] = data["Bcc2"][blockID][x][y][z]
-		#				  values[index * 3 + 2] = data["Bcc3"][blockID][x][y][z]
-		#	  #if blockID%256 == 0:
-		#	  #    print("Dataset" + str(i) + ": done by " + str(blockID/4096.0 * 100) + "%")
-		#	  # for first row: x, so + 0
-		#	  # for second row: y, so + 1
-		#	  # for third row: z, so + 2
-		#print("Done reading.")
-		#dataList = np.array(values).ravel()
-		# Athena stuff ^^^^^
-
-		# for kk in range(16):
-		#	for jj in range(16):
-		#	  for ii in range(16):
-		#		imin = ii * 16
-		#		imax = (ii + 1) * 16
-		#		jmin = jj * 16
-		#		jmax = (jj + 1) * 16
-		#		kmin = kk * 16
-		#		kmax = (kk + 1) * 16
-
-		#		chdatax = Bxdata[imin:imax, jmin:jmax, kmin:kmax]
-		#		chdatay = Bydata[imin:imax, jmin:jmax, kmin:kmax]
-		#		chdataz = Bzdata[imin:imax, jmin:jmax, kmin:kmax]
-
-		#		chdata_vec = np.empty((3, len(chdatax))
-		#		chdata_vec[0] = chdatax
-		#		chdata_vec[1] = chdatay
-		#		chdata_vec[2] = chdataz
-		#		chdata_vec = chdata_vec.T.flatten() # convert to 1D [ Bx(0,0,0), By(0,0,0), Bz(0,0,0), ... ]
-		#		outname = args.dataout + "_{:d}{:d}{:d}.txt".format(ii, jj, kk)
-		#		output_path = os.path.join(args.dataout, outname)
-
-		#		print("Saving to {:s}".format(output_path))
-		#		# TODO: Save as binary
-		#		np.savetxt(output_path, chdata)
-
-
-		#print("Done writing.")
 		timeTaken = time.time() - start_time_sample
 		totalTime = time.time() - start_time_total
 		timeLeft = totalTime / (i+1.0) * (total-(i+1.0))
@@ -244,16 +166,3 @@ if (transformVectorData):
 		Bx.clear_cache()
 		By.clear_cache()
 		Bz.clear_cache()
-
-# single data sets
-#with open('rhoData' + str(data["Time"]) + '.txt', 'wb') as rhoData:
-#	 rhoData.write(bytearray(struct.pack("<i", 1)))
-#	 i = 0
-#	 for block in data["rho"]:
-#		 for zDim in block:
-#				 dataList = np.array(zDim).ravel()
-#				 rhoData.write(bytearray(struct.pack("<%uf" % len(dataList), *dataList)))
-
-
-#for dataset_index, dataset_name in enumerate(data['DatasetNames']):
-
